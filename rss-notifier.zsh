39372d4cec4782b879e93877bec9212f03e53b8b
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2018-11-14 19:36:55 +0330

    Uses -n 0

--- rss-notifier.zsh
+++ rss-notifier.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
-rsstail -l -u "$1" -n 5 | while read -r line1
+rsstail -l -u "$1" -n 0 | while read -r line1
 do
     read -r line2
     if ggrep --silent "$2" <<< "$line1" ; then
-        # Use sth like parallel  --null -N 2 tsend me \"{1} $'\n' {2}\"
+        # Use sth like parallel  --null -N 2 tsend me \"{1} $'\n' {2}\" or ensure-run.zsh "60s" "parallel  --null -N 2 tsend me \\\"{1} $'\n' {2}\\\""
         local c="printf '%b' $line1'\0'$line2|$3"
         # printf "$c"
         eval "$c"
